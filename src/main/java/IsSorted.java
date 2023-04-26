ffb8f0dd0db3a0a19102595c0225aae61c37815a
Sara Aliaga <sarialiaga5@gmail.com>
2023-04-26 20:23:31 +0000

    Completed Challenge: Is Sorted

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,20 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
+        int max= arr[0];
+        int min =arr[0];
+        
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i]==0 || arr[i]==1){
+                return true;
+             }
+             if(arr[i] > max){
+          
+                return true;
+             }  if(arr[i]< min){
+                return false;
+             }
+        }
         return false;
     }
 }
